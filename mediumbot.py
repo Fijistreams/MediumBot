3a4689de925740cfa26767db8988e79d4b89006a
Fijistreams <chollet.dominick@gmail.com>
2021-11-09 23:15:50 -0500

    selenium

--- mediumbot.py
+++ mediumbot.py
@@ -5,6 +5,14 @@ import os
 import discord
 from discord.ext import commands, tasks 
 import datetime
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+#try hardcoding the first class
 
 
 # options = Options()
@@ -15,6 +23,8 @@ mediumbottoken = os.getenv('medium-bot-token')
 #mediumclientid = os.getenv('medium-client-id')
 #mediumclientsecret = os.getenv('medium-client-secret')
 mediumdburl = os.getenv('medium-db-url')
+
+
 
 client = pymongo.MongoClient(mediumdburl)
 db = client['Medium']
@@ -29,50 +39,142 @@ testcounter = 0
 
 def pushTag(tag):
     db.create_collection(tag)
+    taglist = predictTagFilter(tag)
+
+    db['tags'].insert_one({'tag' : tag, 'innerclass' : taglist[0], 'outerclass' : taglist[1]})
 
 def deleteTag(tag):
     db.drop_collection(tag)
 
+def formatClassString(classlist):
+    classstring = ''
+    length = len(classlist)
+    i = 1
+    for x in classlist:
+        classstring = classstring + x
+        if(i < length):
+            classstring = classstring + ' '
+            i = i + 1
+    return classstring
+
+def predictTagFilter(tag):
+    linkclass = None
+    classstring = ''
+    outerclassstring = ''
+    request = re.get('https://medium.com/tag/' + tag + '/top/year')
+    soup = BeautifulSoup(request.content, 'html.parser')
+    for linkhtml in soup.find_all('a'):
+        #if((linkhtml.attrs['href']).startwith('/m/signin') == False and linkhtml.attrs['href'].startwith('https://medium.com') == False):
+        if(str(linkhtml.get('href')).startswith('/m/signin') == False and str(linkhtml.get('href')).startswith('https://medium.com') == False and str(linkhtml.get('href')).startswith('https') == True):
+            if(linkhtml.get('class') != None):
+                linkclass = linkhtml.get('class')
+                classstring = formatClassString(linkclass)
+                print(classstring)
+                break
+    request2 = re.get('https://medium.com/tag/' + tag + '/latest')
+    soup2 = BeautifulSoup(request2.content, 'html.parser')
+
+    for x in soup2.find_all('a', class_ = classstring):
+        classdict = x.parent.attrs
+        #print('hello')
+        try:
+            linkclass = classdict['class']
+            if(len(linkclass) == 2):
+                outerclassstring = formatClassString(linkclass)
+                print(outerclassstring)
+                break
+
+            
+        except Exception:
+            pass
+    return [classstring, outerclassstring]
+
 def getTime():
     nowdate = datetime.datetime.utcnow()
+
+# def predictTagFilter(tag):
+#     request = re.get('https://medium.com/tag/' + tag + '/top/year')
+#     soup = BeautifulSoup(request.content, 'html.parser')
+#     soup.find('a')
+
+def getHTML(tag):
+    options = Options()
+    options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+    options.headless = True
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-sh-usage')
+    options.add_argument("window-size=1920,1080")
+    #driver = webdriver.Chrome(executable_path = "C:\\Users\Dominick\Desktop\chromedriver.exe", options = options)
+    driver = webdriver.Chrome(executable_path= os.environ.get('CHROMEDRIVER_PATH'), chrome_options= options)
+    driver.get('https://medium.com/tag/' + tag + '/latest')
+    
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+
+        # Scroll down to the bottom.
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load the page.
+        time.sleep(5)
+
+        # Calculate new scroll height and compare with last scroll height.
+        new_height = driver.execute_script("return document.body.scrollHeight")
+
+        if new_height == last_height:
+
+            break
+
+        last_height = new_height
+
+    html = driver.page_source
+    return html
 
 def searchtags(tag):
     global db
 
-    headers = {
-       # ':authority' : 'medium.com',
-       # ':method' : 'GET',
-       # ':path' : '/tag/crypto/latest',
-       # ':scheme' : 'https',
-        'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'accept-encoding' : 'gzip, deflate, br',
-        'accept-language' : 'en-US,en;q=0.9',
-        'sec-ch-ua' : '"Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99"',
-        'sec-ch-ua-mobile' : '?1',
-        'sec-ch-ua-platform' : '"Android"',
-        'sec-fetch-dest' : 'document',
-        'sec-fetch-mode' : 'navigate',
-        'sec-fetch-site' : 'none',
-        'sec-fetch-user' : '?1',
-        'upgrade-insecure-requests' : '1',
-        'user-agent' : 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Mobile Safari/537.36'
-    }
+    # classes = db['tags'].find({'tag' : tag}, {'_id' : 0, 'innerclass' : 1, 'outerclass' : 1})
+    # classlist = []
+    # for x in classes:
+    #     classlist.append(x)
 
-    request = re.get('https://medium.com/tag/' + tag + '/latest', headers= headers)
+    # innerclass = classlist[0]['innerclass']
+    # outerclass = classlist[0]['outerclass']
     
-    soup = BeautifulSoup(request.content, 'html.parser')
+    page = getHTML(tag)
+    soup = BeautifulSoup(page, 'html.parser')
+    
     listoflinks = []
+    testcase = ['gv', 'l']
     
+    #All links to articles are currently have this class value
     for link in soup.find_all('a', class_ = 'er es et eu ev ew ex ey ez fa fb fc fd fe ff'):
-        livelink = str(link.get('href'))
-        if(livelink.startswith('/m/signin') == False):
-            if(livelink.startswith('https://') == False):
-                livelink = ('https://medium.com' + livelink)
-            check = db[tag].find({'link' : livelink})
-            if(check.count() == 0):
-                db[tag].insert_one({'link' : livelink})
-                listoflinks.append(livelink)
-                print('yo')
+        classdict = link.parent.attrs
+        try:
+            print(classdict['class'])
+            linkclass = formatClassString(classdict['class'])
+
+        # Links that lead to a sign in are for premium and must be removed
+        #furthermore links without proper https://medium.com must have it added 
+        #Links can be the same but have different query values upon each search so
+        #query values are removed
+        #All is done if the length of the class value of the href tag is 2. This 
+        #ensures that only links to articles are checked and not that of say a button
+            if(len(classdict['class']) == 2):
+                
+                livelink = str(link.get('href'))
+                if(livelink.startswith('/m/signin') == False):
+                    if(livelink.startswith('https://') == False):
+                        livelink = ('https://medium.com' + livelink)
+                    livelink = livelink.split('?', 1)[0]
+                    check = db[tag].find({'link' : livelink})
+                    if(check.count() == 0):
+                        db[tag].insert_one({'link' : livelink})
+                        listoflinks.append(livelink)
+                            #print('yo')    
+        except Exception:
+            pass
+   
     print(listoflinks)
     return listoflinks
 
@@ -123,14 +225,17 @@ async def search():
     listoflinks = []
     stringoflinks = ''
     for x in taglist:
-        resultlist = searchtags(x)
-        for y in resultlist:
-            listoflinks.append(y)
+        if(x != 'tags'):
+            resultlist = searchtags(x)
+            for y in resultlist:
+                listoflinks.append(y)
 
     print(listoflinks)
     
-    for x in listoflinks:
-        await(channel.send(x))
+    #for x in listoflinks:
+        #await(channel.send(x))
+
+
    #     stringoflinks = stringoflinks + x +'\n'
 
   #  if(len(stringoflinks) < 4000 and len(stringoflinks) > 0):
