a0e12256fac9503607289efdb963c97289363547
Fijistreams <chollet.dominick@gmail.com>
2021-11-12 21:53:48 -0500

    Update mediumbot.py

--- mediumbot.py
+++ mediumbot.py
@@ -102,7 +102,10 @@ def getHTML(tag):
     #options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
     options.headless = True
     options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-sh-usage')
+    #options.add_argument('--disable-dev-sh-usage')
+    options.add_argument('enable-automation')
+    options.add_argument('--disable-infobars')
+    options.add_argument('--disable-dev-shm-usage')
     #options.add_argument("window-size=1920,1080")
     #driver = webdriver.Chrome(executable_path = "C:\\Users\Dominick\Desktop\chromedriver.exe", options = options)
     #driver = webdriver.Chrome(executable_path= os.environ.get('CHROMEDRIVER_PATH'), options= options)
