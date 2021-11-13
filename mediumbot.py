5c5fbe9d42dab23a74c6ac2d26ff5978614e45dd
Fijistreams <chollet.dominick@gmail.com>
2021-11-12 22:17:52 -0500

    Update mediumbot.py

--- mediumbot.py
+++ mediumbot.py
@@ -100,7 +100,9 @@ def getTime():
 def getHTML(tag):
     options = Options()
     #options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-    options.headless = True
+    #options.headless = True
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
     #options.add_argument('--disable-dev-sh-usage')
     options.add_argument('enable-automation')
